c0028c6cf20ab52032baf6acfe00175e6ba49406
lipton <liptonmartin135@gmail.com>
2026-09-18 19:32:18 +0300

    lab_1: fiestel cipher (fiestel network)

---
+++ src/Core/Lab_1/FiestelCipher.cs
@@ -0,0 +1,39 @@
+﻿namespace Core.Lab_1;
+
+public class FiestelCipher(IKeyExpander keyExpander, IEncryptionRound encryptionRound)
+{
+    private IKeyExpander KeyExpander => keyExpander;
+    private IEncryptionRound EncryptionRound => encryptionRound;
+
+    public int RoundCount { get; set; } = 16;
+
+    private List<byte[]>? RoundKeys { get; set; }
+
+    public void GenerateRoundKeys(byte[] key)
+    {
+        if (RoundKeys is not null) return;
+
+        RoundKeys = KeyExpander.GenerateRoundKeys(key);
+    }
+
+    public byte[] DoFiestelCipher(byte[] block)
+    {
+        if (RoundKeys is null)
+            throw new InvalidOperationException("Use GenerateRoundKeys method before DoFiestelCipher");
+        
+        var blockSizeBytes = block.Length;
+        var halfBlockSizeBytes = blockSizeBytes / 2;
+            
+        for (var round = 0; round < RoundCount; ++round)
+        {
+            var leftBlock = block[..halfBlockSizeBytes];
+            var rightBlock = block[halfBlockSizeBytes..];
+            
+            leftBlock = EncryptionRound.DoEncrypt(leftBlock, RoundKeys[round]);
+
+            block = rightBlock.Concat(leftBlock).ToArray();
+        }
+
+        return block;
+    }
+}
--- src/Core/Lab_1/Interfaces.cs
+++ src/Core/Lab_1/Interfaces.cs
@@ -1,19 +1,19 @@
 ﻿namespace Core.Lab_1;
 
-public interface IExpandKey
+public interface IKeyExpander
 {
-    public List<byte[]> GenerateRoundKeys(byte[] inputKey);
+    public List<byte[]> GenerateRoundKeys(byte[] key);
 }
 
-public interface IEncryptionStep
+public interface IEncryptionRound
 {
-    public byte[] Encrypt(byte[] inputBlock, byte[] roundKey);
+    public byte[] DoEncrypt(byte[] block, byte[] roundKey);
 }
 
 public interface ISymmetricalEncryptDecrypt
 {
     public int BlockSizeBytes { get; }
-    
+
     public byte[] Encrypt(byte[] block);
 
     public byte[] Decrypt(byte[] block);
